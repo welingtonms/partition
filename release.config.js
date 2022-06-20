1bbab0392b90f8a1b3b60d05326a91f44616bb75
Welington Silva <welington.silva@loadsmart.com>
2022-06-19 23:34:50 -0300

    chore: adjust release assets [skip ci]

--- release.config.js
+++ release.config.js
@@ -1,45 +1,41 @@
 module.exports = {
-  branches: 'main',
-  plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'angular',
-        releaseRules: [
-          { type: 'refactor', release: 'patch' },
-          { type: 'style', release: 'patch' },
-          { type: 'release', release: 'patch' },
-        ],
-        parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-        },
-      },
-    ],
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: true,
-      },
-    ],
-    [
-      '@semantic-release/github',
-      {
-        assets: 'dist/**/*.{js,map}',
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['dist/**/*.{js,map}', 'package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
-  ],
-}
+	branches: 'main',
+	plugins: [
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'angular',
+				releaseRules: [
+					{ type: 'refactor', release: 'patch' },
+					{ type: 'style', release: 'patch' },
+					{ type: 'release', release: 'patch' },
+				],
+				parserOpts: {
+					noteKeywords: [ 'BREAKING CHANGE', 'BREAKING CHANGES' ],
+				},
+			},
+		],
+		'@semantic-release/release-notes-generator',
+		[
+			'@semantic-release/changelog',
+			{
+				changelogFile: 'CHANGELOG.md',
+			},
+		],
+		[
+			'@semantic-release/npm',
+			{
+				npmPublish: true,
+			},
+		],
+		'@semantic-release/github',
+		[
+			'@semantic-release/git',
+			{
+				assets: [ 'package.json', 'CHANGELOG.md' ],
+				message:
+					'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+	],
+};
